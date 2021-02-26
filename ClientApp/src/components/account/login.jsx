6ad6e852124106c460e418b00f6600058f3413a1
Lex Aguilar <alejandro.aguilar@eurekani.com>
2021-02-25 22:41:40 -0600

    iniciar session con enter

--- ClientApp/src/components/account/login.jsx
+++ ClientApp/src/components/account/login.jsx
@@ -24,7 +24,8 @@ function Login(props) {
 
     }
 
-    const sendUser = () => {
+    const sendUser = e => {
+        e.preventDefault();
         setLoading(true);
         userService.login(user)
         .then(userResp => {
@@ -48,22 +49,24 @@ function Login(props) {
         <React.Fragment>
             <ToastContainer autoClose={5000} hideProgressBar />
             <div className="wrapper-login">
-                <form className="form-signin">
+                <form className="form-signin" onSubmit={sendUser}>
                     <div className="imglog">
                         <img className="nav-panel-logo" width={200} src={require('../../svg/logoclinica.png')} />
                     </div>
                     <div style={{ paddingTop: 50 }}>
                         <h4>Iniciar sesión</h4>
-                        <input value={user.username} onChange={onValueChange} type="text" className="form-control" name="username" placeholder="Usuario" required="" autoFocus={true} />
-                        <input value={user.password} onChange={onValueChange} type="password" className="form-control" name="password" placeholder="Contraseña" required="" />
+                        <input value={user.username} onChange={onValueChange} type="text" className="form-control" name="username" placeholder="Usuario" required autoFocus={true} />
+                        <input value={user.password} onChange={onValueChange} type="password" className="form-control" name="password" placeholder="Contraseña" required />
                     </div>
 
                     <Link to={'/account/resetpassword'} >Olvide mi Contraseña</Link>
 
                     <br />
-                    <button className="btn btn-lg btn-primary btn-block" type="button" onClick={sendUser} disabled={loading} >
-                        {loading ? "Iniciando..." : "Login"}
-                    </button>
+                    <input
+                        className="btn btn-lg btn-primary btn-block" 
+                        type="submit" 
+                        value={loading ? "Iniciando..." : "Login"}                         
+                        disabled={loading}/>
                 </form>
             </div>
             <footer className="site__footer">
