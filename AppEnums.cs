142f27765c72044821bc963421ef8d7dbc2360fb
Lex Aguilar <alejandro.aguilar@eurekani.com>
2021-06-08 23:24:05 -0600

    Corregir enumeracion

--- AppEnums.cs
+++ AppEnums.cs
@@ -7,7 +7,7 @@ namespace AtencionClinica
 {    
     public enum InputType { Compras = 1, AjusteEntrada = 2, SaldoInicial = 3, Traslado = 4, Conversion = 5 }
     public enum OutputType { Facturacion = 1, AjusteSalida = 2, FarmaciaServicios = 3, Traslado = 4, Hemodialisis = 6, Conversion = 7 }
-    public enum AreaRestrict { Bodega = 1, Admision = 2, Laboratorio, Farmacia = 7, Emergencia = 8}
+    public enum AreaRestrict { Bodega = 1, Admision = 2, Laboratorio=4, Farmacia = 7, Emergencia = 8}
     public enum Roles {None, Administrador }
     public enum ClientType { Private = 1, Contract = 2 }
     public enum AdmisionTypes { Consulta = 1, IngresoHops = 2 }
